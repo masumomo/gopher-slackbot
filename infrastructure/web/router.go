ee13d35d6d31bf45e4a72342eded36e91487bf5c
masumomo <masumotomiki18@gmail.com>
2020-09-10 07:31:10 +0900

    Modify mock

--- infrastructure/web/router.go
+++ infrastructure/web/router.go
@@ -38,9 +38,9 @@ func newCommandRouter(cc controller.CommandController) func(w http.ResponseWrite
 }
 
 func NewRouter(app *register.App) {
-	http.HandleFunc("/commands", newCommandRouter(app.CommandController))
+	http.HandleFunc("/commands", newEventRouter(app.EventController))
 	http.HandleFunc("/events", newCommandRouter(app.CommandController))
-	http.HandleFunc("/interactions", newCommandRouter(app.CommandController))
+	http.HandleFunc("/interactions", newInteractionRouter(app.InteractionController))
 	if err := http.ListenAndServe(":"+app.Port, nil); err != nil {
 		log.Fatal(err.Error())
 	}
